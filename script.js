33e1af74a223090d330da0c32a94f855fd5a51d7
Anubhav <anubhavverma1195@gmail.com>
2023-08-12 00:50:10 +0530

    firest commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// var count=0;
+// document.getElementById("result").value="Hello Everyone"
+//  document.getElementById("btn").addEventListener("click",()=>{
+
+//     document.getElementById("result").value = document.getElementById("result").value.slice(0,-1);
+//  });
+// Clear the calculator result
+function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  // Delete the last character from the calculator result
+  function deleteLast() {
+    var result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+  }
+  
+  // Append a value to the calculator result
+  function appendToResult(value) {
+    document.getElementById('result').value += value;
+  }
+  
+  // Calculate the result
+  function calculateResult() {
+    var result = document.getElementById('result').value;
+    var calculatedResult = eval(result);
+    document.getElementById('result').value = calculatedResult;
+  }
